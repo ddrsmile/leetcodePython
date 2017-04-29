160590b4fc3463dbea3cd80763003a7f07a7d32e
ddrsmiLe <ddrsmile@gmail.com>
2017-04-30 01:31:33 +0900

    fixed Trees

--- project/utils/Trees.py
+++ project/utils/Trees.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from objs.TreeNode import TreeNode
-from Queue import Queue
+from queue import Queue
 
 class Trees(object):
 
@@ -13,21 +13,21 @@ class Trees(object):
         q = Queue()
         q.put(root)
 
-        half = len(vals)/2
+        half = len(vals)//2
 
         for i in range(half):
-            if not vale[i] == "#":
+            if not vals[i] == "#":
                 c = q.get()
                 left = 2*i + 1
                 right = 2*i + 2
 
                 if not vals[left] == "#":
                     c.left = TreeNode(int(vals[left]))
-                    q.add(c.left)
+                    q.put(c.left)
 
-                if right < len(vals) and  not vals[right] == "#"
+                if right < len(vals) and  not vals[right] == "#":
                     c.right = TreeNode(int(vals[right]))
-                    q.add(c.right)
+                    q.put(c.right)
         return root
 
     def get_BST(self, nums, low, high):
@@ -37,5 +37,5 @@ class Trees(object):
         node = TreeNode(nums[mid])
         node.left = self.get_BST(nums, low, mid - 1)
         node.right = self.get_BST(nums, mid + 1, high)
-        return node;
+        return node
 
