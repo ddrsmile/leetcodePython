08d612fe88ec583b4587a852c8b8d616136cc6f2
ddrsmiLe <ddrsmile@gmail.com>
2017-04-30 01:27:52 +0900

    fixed bug in problem 10

--- project/sols/10.py
+++ project/sols/10.py
@@ -11,7 +11,7 @@ class Solution(object):
         for i in range(1, len(p) + 1):
             for j in range(1, len(s) + 1):
                 if p[i - 1] != '*':
-                    dp[i - 1][j - 1] and (p[i - 1] == s[j - 1] or p[i - 1] == '.')
+                    dp[i][j] = dp[i - 1][j - 1] and (p[i - 1] == s[j - 1] or p[i - 1] == '.')
                 else:
                     dp[i][j] = dp[i - 2][j] or dp[i - 1][j]
                     if p[i - 2] == s[j - 1] or p[i - 2] == '.':
